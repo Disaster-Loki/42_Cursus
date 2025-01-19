1d484aba4a4c73dfd5e502706d4bae8f802aca0c
Disaster-Loki <sebastiaol.carvalho22@gmail.com>
2025-01-19 22:08:33 +0100

    Version 4.6

---
+++ CPP-01/ex06/Harl.cpp
@@ -0,0 +1,50 @@
+#include "Harl.hpp"
+
+Harl::Harl() {}
+
+void Harl::debug() {
+    std::cout << "[ DEBUG ]" << std::endl;
+    std::cout << "I love having extra bacon for my burger. I really do!" << std::endl;
+}
+
+void Harl::info() {
+    std::cout << "[ INFO ]" << std::endl;
+    std::cout << "I cannot believe adding extra bacon costs more money." << std::endl;
+}
+
+void Harl::warning() {
+    std::cout << "[ WARNING ]" << std::endl;
+    std::cout << "I think I deserve to have some extra bacon for free." << std::endl;
+}
+
+void Harl::error() {
+    std::cout << "[ ERROR ]" << std::endl;
+    std::cout << "This is unacceptable! I want to speak to the manager now." << std::endl;
+}
+
+void Harl::complain(std::string level) {
+    std::string levers[] = {"DEBUG", "INFO", "WARNING","ERROR"};
+    void (Harl::*functions[])(void) = {
+        &Harl::debug,
+        &Harl::info,
+        &Harl::warning,
+        &Harl::error
+    };
+    int index = -1;
+    for (int i = 0; i < 4; i++)
+    {
+        if (levers[i] == level)
+        {
+            index = i; break;
+        }
+    }
+    switch (index)
+    {
+    case 0: (this->*functions[0])(); (this->*functions[1])(); (this->*functions[2])();  (this->*functions[3])(); break;
+    case 1: (this->*functions[1])(); (this->*functions[2])(); (this->*functions[3])(); break;
+    case 2: (this->*functions[2])(); (this->*functions[3])(); break;
+    case 3: (this->*functions[3])(); break;
+    default: std::cout << "[ Probably complaining about insignificant problems ] " << std::endl;
+
+    }
+}
