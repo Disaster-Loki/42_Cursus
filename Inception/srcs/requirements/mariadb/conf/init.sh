8cfd1439c5d1d2d80bf53424f89c6d16fa88ac48
Disaster-Loki <sebastiaol.carvalho22@gmail.com>
2025-05-26 10:21:01 -0400

    Version 8.2

--- Inception/srcs/requirements/mariadb/conf/init.sh
+++ Inception/srcs/requirements/mariadb/conf/init.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 set -e
 
-# Inicializa o diretório do banco, se necessário
+# Initialize the database directory if it does not exist
 if [ ! -d /var/lib/mysql/mysql ]; then
-  echo "Inicializando banco de dados..."
+  echo "Initializing database..."
   mysql_install_db --user=mysql --basedir=/usr --datadir=/var/lib/mysql > /dev/null
 
+  # Start MariaDB in safe mode without networking
   mysqld_safe --skip-networking &
   sleep 5
 
-  echo "Configurando root e criando database/usuário..."
+  echo "Setting root password and creating database/user..."
   mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${MARIADB_ROOT_PASSWORD}';"
   mysql -uroot -p${MARIADB_ROOT_PASSWORD} -e "FLUSH PRIVILEGES;"
   mysql -uroot -p${MARIADB_ROOT_PASSWORD} -e "CREATE DATABASE IF NOT EXISTS \`${MARIADB_DATABASE}\`;"
@@ -17,9 +18,10 @@ if [ ! -d /var/lib/mysql/mysql ]; then
   mysql -uroot -p${MARIADB_ROOT_PASSWORD} -e "GRANT ALL PRIVILEGES ON \`${MARIADB_DATABASE}\`.* TO '${MARIADB_USER}'@'%';"
   mysql -uroot -p${MARIADB_ROOT_PASSWORD} -e "FLUSH PRIVILEGES;"
 
+  # Stop the background MariaDB process
   killall mysqld
   sleep 2
 fi
 
-# Executa o comando padrão
+# Execute default command (mysqld)
 exec "$@"
